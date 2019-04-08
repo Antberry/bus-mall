9d8d2e59ca866fa60fef1c5ab9cd28514a7e7586
YOUR FULL NAME <YOUR EMAIL ADDRESS>
2019-04-08 16:13:32 -0700

    got pictures to show randomly

--- js/app.js
+++ js/app.js
@@ -1,12 +1,11 @@
 'use strict';
 
 var allProducts = [];
-var productpic1 = document.getElementById('product1');
-var productpic2 = document.getElementById('product2');
-var productpic3 = document.getElementById('product3');
+var productpics = document.getElementsByClassName('productpics');
+
 
 function ProductPic(name) {
-  this.filepath = `img/${name}.jpg`;
+  this.filepath = `../img/${name}.jpg`;
   this.name = name;
   this.views = 0;
   allProducts.push(this);
@@ -32,3 +31,39 @@ new ProductPic('unicorn');
 new ProductPic('usb');
 new ProductPic('water-can');
 new ProductPic('wine-glass');
+
+function showRandomProd(){
+  var random1 = Math.floor(Math.random() * allProducts.length);
+  var random2 = Math.floor(Math.random() * allProducts.length);
+  var random3 = Math.floor(Math.random() * allProducts.length);
+
+  while (productpics[0].alt === allProducts[random1].name && productpics[1].alt === allProducts[random2].name && productpics[2].alt === allProducts[random3].name){
+    random1 = Math.floor(Math.random() * allProducts.length);
+    random2 = Math.floor(Math.random() * allProducts.length);
+    random3 = Math.floor(Math.random() * allProducts.length);
+
+  }
+  allProducts[random1].views += 1;
+  allProducts[random2].views += 1;
+  allProducts[random3].views += 1;
+  productpics[0].src = allProducts[random1].filepath;
+  productpics[0].alt = allProducts[random1].name;
+  productpics[0].title = allProducts[random1].name;
+  productpics[1].src = allProducts[random2].filepath;
+  productpics[1].alt = allProducts[random2].name;
+  productpics[1].title = allProducts[random2].name;
+  productpics[2].src = allProducts[random3].filepath;
+  productpics[2].alt = allProducts[random3].name;
+  productpics[2].title = allProducts[random3].name;
+}
+
+function handleProdClick(event){
+  console.log(event.target);
+  showRandomProd();
+}
+
+showRandomProd();
+
+productpics[0].addEventListener('click', handleProdClick);
+productpics[1].addEventListener('click', handleProdClick);
+productpics[2].addEventListener('click', handleProdClick);
